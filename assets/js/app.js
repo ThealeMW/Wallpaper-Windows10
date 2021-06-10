faee78c19b0a729a560aae2399aebb1c23ee0a7f
ThealeMW <alexander.ojbro@gmail.com>
2021-06-10 23:12:21 +0200

    Update app.js
    fixed weather&time not updating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
-        $('#datetime').html(moment().format('hh:mm A'));
-        var apiKey = "#"; //MODIFY THIS
-        var displaycity = "#"; //MODIFY THIS - https://openweathermap.org/find
-        var curtemp;
-        var country_name;
-        var weather_description;
-  
-        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + displaycity + "&appid=" + apiKey + "&units=metric", function(data) {
-  
-          city_name = data["name"];
-          country_name = data["sys"]["country"];
-          weather_description = data["weather"][0]["description"];
-          curtemp = data["main"]["temp"];
-  
-          $("#cityname").html(city_name + " &#40;" + country_name + "&#41; " + "<br>" + weather_description);
-          $(".temp").html(curtemp);
-  
-        })
-    })
-    function update() {
-        $('#datetime').html(moment().format('hh:mm A'));
-    }
-setInterval(update, 40000);
+weather();
+timenow();
+})
+function weather() {
+  var apiKey = "#"; //MODIFY THIS
+  var displaycity = "#"; //MODIFY THIS - https://openweathermap.org/find
+  var curtemp;
+  var country_name;
+  var weather_description;
+
+  $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + displaycity + "&appid=" + apiKey + "&units=metric", function(data) {
+
+    city_name = data["name"];
+    country_name = data["sys"]["country"];
+    weather_description = data["weather"][0]["description"];
+    curtemp = data["main"]["temp"];
+
+    $("#cityname").html(city_name + " &#40;" + country_name + "&#41; " + "<br>" + weather_description);
+    $(".temp").html(curtemp + " &#8451;");
+
+  })
+}
+function timenow() {
+  $('#datetime').html(moment().format('hh:mm A'));
+}
+  setInterval(weather, 1000 * 60 * 60);
+  setInterval(timenow, 1000);
